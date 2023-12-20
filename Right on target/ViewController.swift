6faac0eaf4d9523cc08a81285b4d90a0c631908a
homeless <hmlssgd@gmail.com>
2023-12-20 18:04:14 +0300

    Initial Commit

---
+++ Right on target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on target
+//
+//  Created by MacBookAir on 20.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
