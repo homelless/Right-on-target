98eba79fdff50ebc246664ad932235f362a04d8b
homeless <hmlssgd@gmail.com>
2023-12-23 01:49:29 +0300

    3.5

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -12,7 +12,51 @@ class ViewController: UIViewController {
     @IBOutlet var slider : UISlider!
     @IBOutlet var label : UILabel!
     
+    lazy var secondViewController: SecondViewController = getSecondViewController()
     
+    private func getSecondViewController() -> SecondViewController{
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+     
+        let viewController = storyboard.instantiateViewController(identifier:
+        "SecondViewController")
+        return viewController as! SecondViewController
+    }
+    
+    @IBAction func showNextScreen(){
+        self.present(secondViewController, animated: true, completion: nil)
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+        
+        let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+        versionLabel.text = "Версия 1.1"
+        self.view.addSubview(versionLabel)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
