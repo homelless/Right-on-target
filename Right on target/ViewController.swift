5e8d3bdb64ea174941fe61fd1091f683691447c5
homeless <hmlssgd@gmail.com>
2023-12-22 16:12:09 +0300

    1.3

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slider : UISlider!
+    @IBOutlet var label : UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
